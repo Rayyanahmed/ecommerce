293db43a6ade04f7d8ae1c0fd3da2df34eb0341b
Rayyan Ahmed <rayyan.z.ahmed@gmail.com>
2016-01-22 10:41:21 -0500

    added query function

--- public/resources/functions.php
+++ public/resources/functions.php
@@ -4,5 +4,12 @@ function redirect($location) {
 	header("Location: $location");
 }
 
+function query($sql) {
+	// if we want to bring in the $connection variable from our config file into
+	// this function must declare global, otherwise it will create a different variable
+	global $connection;
+	return mysqli_query($connection, $sql)
+}
+
 
 ?>
