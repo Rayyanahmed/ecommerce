e5c80768e88b8471102319a59525d8eea8fa2a6f
Rayyan Ahmed <rayyan.z.ahmed@gmail.com>
2016-01-22 10:24:36 -0500

    terminate database incase query fails

--- public/resources/templates/front/side_nav.php
+++ public/resources/templates/front/side_nav.php
@@ -5,6 +5,10 @@
     		$query = "SELECT * FROM categories";
     		$send_query = mysqli_query($connection, $query);
 
+    		if(!$send_query) {
+    			die("QUERY FAILED" . mysqli_error($connection));
+    		}
+
     		while ($row = mysqli_fetch_assoc($send_query)) {
     			 echo "<a href='#' class='list-group-item'>" . $row['cat_title'] . "</a>";
     		}
