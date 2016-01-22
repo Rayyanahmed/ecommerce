363f1c752832bc9e6d9d484c071fef7feb47aa96
Rayyan Ahmed <rayyan.z.ahmed@gmail.com>
2016-01-22 12:40:18 -0500

    refactored fetching categories into helper function

--- public/resources/functions.php
+++ public/resources/functions.php
@@ -63,4 +63,15 @@ echo $product;
 	}
 }
 
+
+function get_categories() {
+	global $connection;
+	$query = "SELECT * FROM categories";
+	$send_query = mysqli_query($connection, $query);
+
+	while ($row = fetch_array($send_query)) {
+		 echo "<a href='#' class='list-group-item'>" . $row['cat_title'] . "</a>";
+	}
+}
+
 ?>
--- public/resources/templates/front/side_nav.php
+++ public/resources/templates/front/side_nav.php
@@ -2,13 +2,7 @@
     <p class="lead">Shop Name</p>
     <div class="list-group">
     	<?php
-    		$query = "SELECT * FROM categories";
-    		$send_query = mysqli_query($connection, $query);
-
-    		while ($row = fetch_array($send_query)) {
-    			 echo "<a href='#' class='list-group-item'>" . $row['cat_title'] . "</a>";
-    		}
-
+    		get_categories();
     	 ?>
     </div>
 </div>
