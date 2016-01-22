7d1ccc4e35e93b630672119823fbc184c9eb76ab
Rayyan Ahmed <rayyan.z.ahmed@gmail.com>
2016-01-22 13:01:43 -0500

    added category links

--- public/resources/functions.php
+++ public/resources/functions.php
@@ -70,7 +70,7 @@ $query = query("SELECT * FROM categories");
 confirm($query);
 while ($row = fetch_array($query)) {
 $category_links = <<<DELIMETER
-	<a href='category.php' class='list-group-item'>{$row['cat_title']}</a>
+	<a href='category.php?id={$row['cat_id']}' class='list-group-item'>{$row['cat_title']}</a>
 DELIMETER;
 echo $category_links;
 	}
