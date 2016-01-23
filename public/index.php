bc0bb6cb0b424eff4b3d524ff8398a0a7fadef3c
Rayyan Ahmed <rayyan.z.ahmed@gmail.com>
2016-01-23 18:13:59 -0500

    added session destroy to delete cart increment

--- public/index.php
+++ public/index.php
@@ -19,11 +19,6 @@
 
                 <div class="row">
 
-                    <h1>
-                        <?php echo $_SESSION['product_1']; ?>
-
-                    </h1>
-
                     <?php get_products(); ?>
                 </div><!-- Row ends here -->
 
