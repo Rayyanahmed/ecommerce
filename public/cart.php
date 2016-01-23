eb9cea0c61a54da5c4f2167a1c15e6b29b12ae27
Rayyan Ahmed <rayyan.z.ahmed@gmail.com>
2016-01-23 17:59:37 -0500

    notes on cart functionality

--- public/cart.php
+++ public/cart.php
@@ -1,1 +1,12 @@
 <?php require_once("./resources/config.php"); ?>
+
+
+<?php 
+// First thing incoming is the GET request
+
+if(isset($_GET['add'])) {
+  $_SESSION['product_' . $_GET['add']] += 1;
+}
+echo $_SESSION['product_1'];
+
+?>
