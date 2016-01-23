9f5b5479a71b3cd65848b5c30b99cd267395a122
Rayyan Ahmed <rayyan.z.ahmed@gmail.com>
2016-01-23 18:21:25 -0500

    first query for all products available that match requested product

--- public/cart.php
+++ public/cart.php
@@ -5,8 +5,13 @@
 // First thing incoming is the GET request
 
 if(isset($_GET['add'])) {
-  $_SESSION['product_' . $_GET['add']] += 1;
-  redirect("index.php");
+  // Here we are building a cap according to how many products we have on hand
+  $query = query("SELECT * FROM products WHERE product_id =" . escape_string($_GET['add'] . " "));
+  // Make sure query goes through
+  confirm($query);
+
+  // $_SESSION['product_' . $_GET['add']] += 1;
+  // redirect("index.php");
 }
 
 
