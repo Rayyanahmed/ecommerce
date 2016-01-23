ecef0af64effe9f8660f7b10f9d7bfe537cc5bbf
Rayyan Ahmed <rayyan.z.ahmed@gmail.com>
2016-01-23 13:06:00 -0500

    took out footer

--- public/category.php
+++ public/category.php
@@ -83,14 +83,7 @@
 
         <hr>
 
-        <!-- Footer -->
-        <footer>
-            <div class="row">
-                <div class="col-lg-12">
-                    <p>Copyright &copy; Your Website 2030</p>
-                </div>
-            </div>
-        </footer>
+  
 
     </div>
     <!-- /.container -->
