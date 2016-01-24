1b7361864dc02538e514c1bc6851bbe1968dd914
Rayyan Ahmed <rayyan.z.ahmed@gmail.com>
2016-01-23 19:15:26 -0500

    added to checkout

--- public/cart.php
+++ public/cart.php
@@ -10,8 +10,16 @@ if(isset($_GET['add'])) {
   // Make sure query goes through
   confirm($query);
 
-  // $_SESSION['product_' . $_GET['add']] += 1;
-  // redirect("index.php");
+  while($row = fetch_array($query)) {
+    if($row['product_quantity'] != $_SESSION['product_' . $_GET['add']]) {
+      $_SESSION['product_' . $_GET['add']] += 1;
+    } else {
+      set_message("We only have " . $row['product_quantity'] . " available");
+      redirect("checkout.php");
+    }
+  }
+
+
 }
 
 
