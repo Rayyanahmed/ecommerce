c981e6328db90f874bc1c57d8e0da429af3b9b0d
Rayyan Ahmed <rayyan.z.ahmed@gmail.com>
2016-01-24 18:11:29 -0500

    added paypal form

--- public/checkout.php
+++ public/checkout.php
@@ -13,7 +13,9 @@
 
       <h1>Checkout</h1>
 
-<form action="">
+<form action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="post">
+  <input type="hidden" name="cmd" value="_cart">
+  <input type="hidden" name="business" value="rayyan.z.ahmed-facilitator@gmail.com">
     <table class="table table-striped">
         <thead>
           <tr>
@@ -30,6 +32,8 @@
             </tr>
         </tbody>
     </table>
+    <input type="image" name="submit" border="0" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynow_LG.gif" 
+    alt="PayPal - The safer, easier way to pay online">
 </form>
 
 
