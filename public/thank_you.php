2fd23622444c0d1436adcafe1aba287d155ae56a
Rayyan Ahmed <rayyan.z.ahmed@gmail.com>
2016-01-24 22:14:28 -0500

    adding thank you page

---
+++ public/thank_you.php
@@ -0,0 +1,12 @@
+<?php require_once("./resources/config.php"); ?>
+<?php require_once("cart.php") ?>
+<?php include (TEMPLATE_FRONT . DS . "header.php"); ?>
+
+
+    <div class="container">
+
+    <div>
+
+
+
+<?php include (TEMPLATE_FRONT . DS . "footer.php"); ?>
