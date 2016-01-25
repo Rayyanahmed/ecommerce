66e5a3077e74df31b6ef59af087c17d9d7145b85
Rayyan Ahmed <rayyan.z.ahmed@gmail.com>
2016-01-24 22:20:41 -0500

    forgot to add currency

--- public/checkout.php
+++ public/checkout.php
@@ -16,6 +16,7 @@
 <form action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="post">
   <input type="hidden" name="cmd" value="_cart">
   <input type="hidden" name="business" value="rayyan.z.ahmed-facilitator@gmail.com">
+  <input type="hidden" name="currency_code" value="US"> 
     <table class="table table-striped">
         <thead>
           <tr>
